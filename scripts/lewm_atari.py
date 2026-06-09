4927e40cdab9405989fdd9ab8bb914140ed8f42e
Vivswan Shitole <vivs2904@gmail.com>
2026-06-10 02:47:13 +0530

    updates script

--- scripts/lewm_atari.py
+++ scripts/lewm_atari.py
@@ -1,5 +1,11 @@
 '''
-This is a minimal adaptation of LeWorldModel to online setting in the Atari environment. The dynamics model is learnt in spirit with LeWorldModel: a CNN encoder encodes observations into embeddings and a gru based predictor predicts the embedding of the current observation conditioned on previous observations and actions. The encoder and predictor are trained using prediction and sigreg losses. The behaviour model is learnt using the Dreamer-v3 famerwork: a reward model and a discount model are learnt as probes over the learnt dynamics model in a detached state. Actor and Critic models are trained using imagined rollouts over the learnt dynamics model
+This is a minimal adaptation of LeWorldModel to online setting in the Atari environment. 
+The dynamics model is learnt in spirit with LeWorldModel: 
+a CNN encoder encodes observations into embeddings and a gru based predictor predicts the embedding of the current observation conditioned on previous observations and actions. 
+The encoder and predictor are trained using prediction and sigreg losses.
+The behaviour model is learnt using the Dreamer-v3 famerwork: 
+a reward model and a discount model are learnt as probes over the learnt dynamics model in a detached state. 
+Actor and Critic models are trained using imagined rollouts over the learnt dynamics model.
 
 '''
 
